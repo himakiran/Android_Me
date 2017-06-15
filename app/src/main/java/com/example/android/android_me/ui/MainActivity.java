0e16be391fa71a09803852d195b4c82e8b0fdc51
himakiran <himakiran@gmail.com>
2017-06-15 17:15:00 +0530

    implemented interface so that master list fragment and main activity can communicate

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -18,18 +18,26 @@ package com.example.android.android_me.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.android.android_me.R;
 
 // This activity is responsible for displaying the master list of all images
 // TODO (4) Implement the MasterListFragment callback, OnImageClickListener
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener{
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+
+    public void onImageSelected(int position) {
+
+        Toast.makeText(getApplicationContext(),"pos clicked : " + position, Toast.LENGTH_SHORT).show();
 
     }
 
